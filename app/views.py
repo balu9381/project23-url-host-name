4a8131ab880fab8b80e8fd375b2ae44c1a4eb204
balu9381 <baluravi215@gmail.com>
2023-01-16 11:31:42 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,5 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+# Create your views here.
+def balu(request):
+    return HttpResponse('<marque><h1>THIS IS BALA 192.168.203.48 PAGE</marque></h1>')
